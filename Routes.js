a6ae67fa980dbca028af910049c77d867cd4cdee
ryydawg10 <152838976+ryydawg10@users.noreply.github.com>
2024-08-20 04:59:48 -0700

    Create Routes.js

---
+++ Routes.js
@@ -0,0 +1,18 @@
+const handleRequest = (req, res) => {
+        if (req.url === '/api/sends/4hr') {
+            
+        } else if (req.url === '/api/sends/1day') {
+
+        } else if (req.url === '/api/sends/4day') {
+
+        } else if (req.url === '/api/sends/8days') {
+
+        } else if (req.url === '/api/sends/15days') {
+
+        } else if (req.url === '/api/sends/30days') {
+
+        }
+            
+        };
+
+module.exports = handleRequest;
